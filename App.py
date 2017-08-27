2fa9733aea23d68b0eefe61a685a41ce637aab86
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2017-08-27 11:51:46 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,18 @@
 import json
+def menu():
+    print("1- Adicionar contato")
+    print("2- Editar contato")
+    print("3-Pesquisar contato")
+    print("4-Visualisar lista de contatos")
+    print("5-Aapagar contato")
+    print("6- Sair")
+
+ 
+while True:
+    opção = menu()
+    if opção == 6
+        break
+
 def main():
     while True:
         try:
