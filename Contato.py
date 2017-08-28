9b6c468d3f880502e6a6a1746199cfd24d37724d
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2017-08-28 12:44:45 -0300

    Update Contato.py

--- Contato.py
+++ Contato.py
@@ -1,9 +1,12 @@
 import datetime
 class Contato():
     def __init__(self, pessoa, telefones):
-        self.criacao = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+        self.criacao = datetime.datetime().strftime("%y-%m-%d")
         self.pessoa = pessoa
         self.telefones = telefones
 
     def listarTelefone(self):
-        pass
+        for Telefone in self.Telefone:
+            print (Telefone)
+        def __str__ (self):
+            return"%s" % (self.Telefone)
