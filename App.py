47cf4b3a2205e5431a9f4981d86279a2542ba9dc
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2017-08-28 09:17:49 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,31 +1,38 @@
 import json
+from Agenda import Agenda
+from Contato import Contato
+from Pessoa import Pessoa
+from Telefone import Telefone
+
 def menu():
     print("1- Adicionar contato")
-    print("2- Editar contato")
+    print("2-Listar Contatos")
     print("3-Pesquisar contato")
-    print("4-Visualisar lista de contatos")
-    print("5-Aapagar contato")
+    print("4- Quantidade de contatos")
+    print("5- Remover contato")
     print("6- Sair")
 
- 
 while True:
     opção = menu()
-    if opção == 6
+    if opção == 6:
         break
+    elif opção == 1:
+        Agenda()
+    elif opção == 2:
+        Editar()
+    elif opção == 3:
+        pesquisa2()
+    elif opção == 4:
+        listar()
+    elif opção == 5:
+        apagar()
 
 def main():
-    while True:
-        try:
-            f = open("meses.txt", encoding="utf8")
-            linhas = f.readlines()
 
-            for linha in linhas:
-                print(linha.strip())
+    a1 = Agenda("sajdn")
+    a1.AdicionaContado(Contato(Pessoa("Rennan", "23/34/323245", "hotmail").__dict__, [Telefone("8888", 83, "+55").__dict__]).__dict__)
+    a1.salvarContatos()
 
-            break
-
-        except FileNotFoundError:
-            print("arquivo não encontrado")
 
 
 if __name__ == '__main__':
