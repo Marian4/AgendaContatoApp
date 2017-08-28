b3374b2caefb93fd911027032ba6feb9980135d5
Sabriny Bezerra <sabrinypereirabezerra@gmail.com>
2017-08-28 12:46:42 -0300

    Update App.py

--- App.py
+++ App.py
@@ -4,35 +4,36 @@ from Contato import Contato
 from Pessoa import Pessoa
 from Telefone import Telefone
 
-def menu():
-    print("1- Adicionar contato")
-    print("2-Listar Contatos")
-    print("3-Pesquisar contato")
-    print("4- Quantidade de contatos")
-    print("5- Remover contato")
-    print("6- Sair")
-
-while True:
-    opção = menu()
-    if opção == 6:
-        break
-    elif opção == 1:
-        Agenda()
-    elif opção == 2:
-        Editar()
-    elif opção == 3:
-        pesquisa2()
-    elif opção == 4:
-        listar()
-    elif opção == 5:
-        apagar()
-
 def main():
-
     a1 = Agenda("sajdn")
-    a1.AdicionaContado(Contato(Pessoa("Rennan", "23/34/323245", "hotmail").__dict__, [Telefone("8888", 83, "+55").__dict__]).__dict__)
-    a1.salvarContatos()
 
+    #a1.AdicionaContado(Contato(Pessoa("Rennan", "23/34/323245", "hotmail").__dict__, [Telefone("8888", 83, "+55").__dict__]).__dict__)
+    #a1.salvarContatos()
+
+
+
+    while True:
+        print("Escolha uma opção: ")
+        print("1- Adicionar contato")
+        print("2- Listar Contatos")
+        print("3- Pesquisar contato")
+        print("4- Quantidade de contatos")
+        print("5- Remover contato")
+        print("6- Sair")
+        opcao = int(input())
+
+        if opcao == 1:
+            a1.AdicionaContado(Pessoa)
+        elif opcao == 2:
+            a1.ListarContato()
+        elif opcao == 3:
+            a1.pesquisar()
+        elif opcao == 4:
+            a1.QuantContatos()
+        elif opcao == 5:
+            a1.ExcluirContato()
+        elif opcao == 6:
+            break
 
 
 if __name__ == '__main__':
